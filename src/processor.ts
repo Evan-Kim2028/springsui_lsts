a29bc13e7c15c5c9658115ef40df1e3fec9d191e
Evan K <ekcopersonal@gmail.com>
2024-11-29 01:16:56 -0500

    refactor: remove redundancies in spring_sui_events

--- src/processor.ts
+++ src/processor.ts
@@ -3,35 +3,36 @@ import { events as afsui_events } from './types/sui/0x7f6ce7ade63857c4fd16ef7783
 import { liquid_staking as spring_sui_events } from './types/sui/0xb0575765166030556a6eafd3b1b970eba8183ff748860680245b9edd41c716e7.js';
 import { _0x1 } from '@typemove/sui/builtin';
 
+// TODO - Refactor afsui_events and spring_sui_events into separate files 
 // afsui events
-afsui_events
-  .bind()
-  .onEventStakedEvent((event: afsui_events.StakedEventInstance, ctx: SuiContext) => {
-    ctx.meter.Counter('staked_events').add(1);
-    ctx.eventLogger.emit('afsui_StakedEvent', {
-      distinctId: event.data_decoded.staker,
-      staker: event.data_decoded.staker,
-      validator: event.data_decoded.validator,
-      staked_sui_id: event.data_decoded.staked_sui_id,
-      sui_id: event.data_decoded.sui_id,
-      sui_amount: event.data_decoded.sui_amount.toString(),
-      afsui_id: event.data_decoded.afsui_id,
-      afsui_amount: event.data_decoded.afsui_amount.toString(),
-      validator_fee: event.data_decoded.validator_fee.toString(),
-      referrer: event.data_decoded.referrer,
-      epoch: event.data_decoded.epoch.toString(),
-      is_restaked: event.data_decoded.is_restaked,
-    });
-  })
-  .onEventUnstakeRequestedEvent((event: afsui_events.UnstakeRequestedEventInstance, ctx: SuiContext) => {
-    ctx.meter.Counter('unstake_requested_events').add(1);
-    ctx.eventLogger.emit('afsui_UnstakeRequestedEvent', {
-      afsui_id: event.data_decoded.afsui_id,
-      provided_afsui_amount: event.data_decoded.provided_afsui_amount.toString(),
-      requester: event.data_decoded.requester,
-      epoch: event.data_decoded.epoch.toString(),
-    });
-  });
+// afsui_events
+//   .bind()
+//   .onEventStakedEvent((event: afsui_events.StakedEventInstance, ctx: SuiContext) => {
+//     ctx.meter.Counter('staked_events').add(1);
+//     ctx.eventLogger.emit('afsui_StakedEvent', {
+//       distinctId: event.data_decoded.staker,
+//       staker: event.data_decoded.staker,
+//       validator: event.data_decoded.validator,
+//       staked_sui_id: event.data_decoded.staked_sui_id,
+//       sui_id: event.data_decoded.sui_id,
+//       sui_amount: event.data_decoded.sui_amount.toString(),
+//       afsui_id: event.data_decoded.afsui_id,
+//       afsui_amount: event.data_decoded.afsui_amount.toString(),
+//       validator_fee: event.data_decoded.validator_fee.toString(),
+//       referrer: event.data_decoded.referrer,
+//       epoch: event.data_decoded.epoch.toString(),
+//       is_restaked: event.data_decoded.is_restaked,
+//     });
+//   })
+//   .onEventUnstakeRequestedEvent((event: afsui_events.UnstakeRequestedEventInstance, ctx: SuiContext) => {
+//     ctx.meter.Counter('unstake_requested_events').add(1);
+//     ctx.eventLogger.emit('afsui_UnstakeRequestedEvent', {
+//       afsui_id: event.data_decoded.afsui_id,
+//       provided_afsui_amount: event.data_decoded.provided_afsui_amount.toString(),
+//       requester: event.data_decoded.requester,
+//       epoch: event.data_decoded.epoch.toString(),
+//     });
+//   });
 
 
 // springsui events
@@ -45,10 +46,7 @@ spring_sui_events
       sui_amount_in: event.data_decoded.sui_amount_in.toString(),
       fee_amount: event.data_decoded.fee_amount,
     });
-  });
-
-spring_sui_events
-  .bind()
+  })
   .onEventRedeemEvent((event: spring_sui_events.RedeemEventInstance, ctx: SuiContext) => {
     ctx.meter.Counter('mint_events').add(1);
     ctx.eventLogger.emit('spring_sui_Mint', {
